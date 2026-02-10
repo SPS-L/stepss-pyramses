55c587dc45b7cf0ae445db0f4a124755251e6802
Petros Aristidou <apetros@pm.me>
2026-02-10 13:26:53 +0200

    Update version to 0.0.60 and remove obsolete .gitignore file

--- src/pyramses/__init__.py
+++ src/pyramses/__init__.py
@@ -3,7 +3,7 @@
 """Python library for RAMSES dynamic simulator."""
 
 __name__ = "pyramses"
-__version__ = '0.0.50'
+__version__ = '0.0.60'
 __author__ = "Petros Aristidou"
 __copyright__ = "Petros Aristidou"
 __license__ = "Petros Aristidou"
